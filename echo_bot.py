ec5e10fa6efd82aa00f85a2d2a0b0b8e17fd479a
RoboDJLex <djlex2013@mail.ru>
2022-10-28 23:52:38 +0300

    commit

--- echo_bot.py
+++ echo_bot.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 import telebot
 from telebot import types
 import models
@@ -12,10 +13,12 @@ def start(m):
         markup.add(item1)
         markup.add(item2)
         bot.send_message(m.chat.id, 'Привет!\nСоздай анкету.',  reply_markup=markup)
-
+user = models.User
+user.soft_skills = NULL
+user.hard_skills = NULL
+user.character = NULL
 @bot.message_handler(content_types=["text"])
 def hadle_text(m):
-        user = models.User
         user.id = m.chat.id
         if m.text.strip() == 'Ввести имя' or m.text.strip() == 'Изменить имя':
                 msg = bot.send_message (m.chat.id, 'Введите Фамилию и Имя', reply_markup=types.ReplyKeyboardRemove())
@@ -46,6 +49,8 @@ def hadle_text(m):
                 markup.add(item10)
                 markup.add(item11)
                 bot.send_message (m.chat.id, 'Выберите подходящие Вам софт-скилы', reply_markup=markup)
+                while m.text.strip()!= 'Далее':
+                        user.soft_skills.append(m.text)
         if m.text.strip() == 'Далее':
                 markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
                 item1=types.KeyboardButton("1")
@@ -71,6 +76,8 @@ def hadle_text(m):
                 markup.add(item10)
                 markup.add(item11)
                 bot.send_message (m.chat.id, 'Выберите подходящие Вам хард-скилы', reply_markup=markup)
+                while m.text.strip()!= 'Следубщее':
+                        user.hard_skills.append(m.text)
         if m.text.strip() == 'Следующее':
                 markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
                 item1=types.KeyboardButton("1")
@@ -96,14 +103,17 @@ def hadle_text(m):
                 markup.add(item10)
                 markup.add(item11)
                 bot.send_message (m.chat.id, 'Выберите подходящие Вам личностные качества', reply_markup=markup)
-                        
+                while m.text.strip()!= 'Завершить':
+                        user.character.append(m.text)
+        return UserData
+
 def UserData(m):
         markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
         item1=types.KeyboardButton("Изменить имя")
         item2=types.KeyboardButton("Добавить теги")
         markup.add(item1)
         markup.add(item2)
-        bot.send_message (m.chat.id, 'Вас зовут:'+m.text, reply_markup=markup)
+        bot.send_message (m.chat.id, 'Ваш ID: '+user.id+'\nВас зовут: '+user.fullname+'\nВаши софт-скилы: '+user.soft_skills+'\nВаши хард-скилы: '+user.hard_skills+'\nВаши личностные качества: '+user.character, reply_markup=markup)
 
 bot.infinity_polling()
 
