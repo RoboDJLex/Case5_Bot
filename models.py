8be4176f90aaa20d5aa67861a32fdccd795030a1
Danila Eliseev <danila.eliseev@altenar.com>
2022-10-28 22:04:53 +0300

    skills model

--- models.py
+++ models.py
@@ -8,13 +8,6 @@ class User(BaseModel):
     soft_skills: List[str]
     character: List[str]
 
-external_data = {'id': 1,
-                 'fullname': 'Suka Yeba',
-                 'hard_skills': ['Python','HTML'],
-                 'soft_skills': ['Communicative'],
-                 'character': ['Sexy', 'Stylish']   
-}
-
-new_user = User(**external_data)
-
-print(new_user.hard_skills[1])
+class UserSkills(BaseModel):
+    type: str
+    list: List[str]
