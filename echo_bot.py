f2df770f33855d60aefa73c5dd4467aab73b2881
RoboDJLex <djlex2013@mail.ru>
2022-10-28 17:23:18 +0300

    commit

--- echo_bot.py
+++ echo_bot.py
@@ -1,4 +1,3 @@
-from email.policy import default
 import telebot
 from telebot import types
 
@@ -7,18 +6,16 @@ bot = telebot.TeleBot("5620161730:AAEHy5MphcICV2Uo8izjqkAArQvGUL1vOXI", parse_mo
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
         markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1=types.KeyboardButton("Ввести имя", callback_data = "1")
-        item2=types.KeyboardButton("Добавить тег", callback_data = "2")
+        item1=types.KeyboardButton("Ввести имя")
+        item2=types.KeyboardButton("Добавить тег")
         markup.add(item1)
         markup.add(item2)
         bot.send_message(m.chat.id, 'Привет!\nСоздай анкету.',  reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda call:True)
-def callback_query(call):
-        req = call.data.split('_')
-        if req == '1':
-                bot.send_message(m.chat.id, 'Введите имя')
-        elif  req == '2':
-                bot.send_message(m.chat.id, 'Введите теги')
-
+@bot.message_handler(content_types=["text"])
+def hadle_text(m):
+        if m.text.strip() == 'Ввести имя':
+                bot.send_message (m.chat.id, 'Введи Фамилию и Имя')
+        elif m.text.strip() == 'Добавить тег':
+                bot.send_message (m.chat.id, 'Введи Теги')
 bot.infinity_polling()
