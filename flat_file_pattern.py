6dc8d449c2f9ca15db15e43fee05595493597ffa
Danila Eliseev <danila.eliseev@altenar.com>
2022-10-29 02:17:48 +0300

    register users in base

--- flat_file_pattern.py
+++ flat_file_pattern.py
@@ -1,4 +1,70 @@
 from openpyxl import *
+import models
 
-users = load_workbook('Users.xlsx')
 
+base = load_workbook('Users.xlsx')
+
+base.active
+
+users = base['Users']
+
+
+def get_users(users):
+
+    output = []
+
+    i = 0
+
+    j = 0
+
+    fields = ['id','fullname','hard_skills','soft_skills','character']
+
+    for row in users.rows:
+        
+        user = models.User
+        
+        data = {
+            'id': '',
+            'fullname': '',
+            'hard_skills': '',
+            'soft_skills': '',
+            'character': ''
+        }
+
+        for cell in row:
+            data[fields[j]] = cell.value
+            j += 1
+        
+        output.append(data)
+        j = 0
+        i += 1
+    
+    base.close()
+
+    return output
+
+def register_user(user: models.User):
+
+    rows = ['A','B','C','D','E']
+
+    fields = ['id','fullname','hard_skills','soft_skills','character']
+
+    j = 0
+
+    position = users.max_row + 1
+
+    for row in rows:
+        users[f'{row}{position}'] = user[fields[j]]
+        j += 1
+
+    base.save('Users.xlsx')
+
+pavlik = {
+    'id': 12332,
+    'fullname': 'Fuck Pasha',
+    'hard_skills': "['Python','Html']",
+    'soft_skills': "['Python','Html']",
+    'character': "['Python','Html']"
+}
+
+register_user(pavlik)
